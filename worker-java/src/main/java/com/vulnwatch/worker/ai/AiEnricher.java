ccba3597440a2961d1771c39a36cd800df7fd8d3
mouse1999 <kufreedward26@gmail.com>
2026-05-11 23:18:20 +0100

    feat: add initial Ai class

---
+++ worker-java/src/main/java/com/vulnwatch/worker/ai/AiEnricher.java
@@ -0,0 +1,9 @@
+package com.vulnwatch.worker.ai;
+
+import com.vulnwatch.worker.models.AggregatedScanData;
+import com.vulnwatch.worker.models.ai.EnrichedScanResult;
+
+public class AiEnricher {
+    public EnrichedScanResult enrich(AggregatedScanData aggregated) {
+    }
+}
