b675cf68a851953d83acf2b0553845f8640cff2d
mouse1999 <kufreedward26@gmail.com>
2026-05-11 14:39:13 +0100

    feat(api): add OpenAPI documentation to interfaces

--- worker-java/src/main/java/com/vulnwatch/worker/enums/FindingStatus.java
+++ worker-java/src/main/java/com/vulnwatch/worker/enums/FindingStatus.java
@@ -1,7 +1,19 @@
 package com.vulnwatch.worker.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * Status of a security finding in the remediation workflow.
+ */
+@Schema(description = "Status of a security finding in the remediation workflow")
 public enum FindingStatus {
+
+    @Schema(description = "Finding has been identified and not yet fixed")
     OPEN,
+
+    @Schema(description = "Finding has been fixed via PR or manual action")
     REMEDIATED,
+
+    @Schema(description = "Finding has been marked as not applicable or acceptable risk")
     IGNORED
 }
--- worker-java/src/main/java/com/vulnwatch/worker/enums/SurfaceType.java
+++ worker-java/src/main/java/com/vulnwatch/worker/enums/SurfaceType.java
@@ -1,8 +1,27 @@
 package com.vulnwatch.worker.enums;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+/**
+ * Surface types that can be scanned by different scanners.
+ * Each scanner returns results for one surface type.
+ */
+@Schema(description = "Type of security surface being scanned")
 public enum SurfaceType {
+
+    @Schema(description = "DNS records and configurations (SPF, DMARC, DNSSEC, etc.)")
     DNS,
+
+    @Schema(description = "SSL/TLS certificates, protocols, and cipher suites")
     SSL,
+
+    @Schema(description = "HTTP security headers (HSTS, CSP, X-Frame-Options, etc.)")
     HTTP_HEADERS,
-    DEPENDENCY
+
+    @Schema(description = "Dependency vulnerabilities (npm, maven, pip packages)")
+    DEPENDENCY,
+
+    @Schema(description = "Hardcoded secrets, API keys, and credentials")
+    SECRETS,
+
 }
