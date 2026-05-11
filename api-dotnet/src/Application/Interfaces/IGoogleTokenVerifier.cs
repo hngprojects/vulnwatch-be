db035d7ae8f0591beb58e93353089f180d7bdf5a
D.C. <d-grid@outlook.com>
2026-05-11 07:56:45 +0100

    Fix casing of parameter 'idToken' to 'IdToken'

--- api-dotnet/src/Application/Interfaces/IGoogleTokenVerifier.cs
+++ api-dotnet/src/Application/Interfaces/IGoogleTokenVerifier.cs
@@ -4,7 +4,7 @@ namespace Application.Interfaces;
 
 public interface IGoogleTokenVerifier
 {
-    Task<Result<GoogleUserInfo>> VerifyIdTokenAsync(string idToken, CancellationToken ct);
+    Task<Result<GoogleUserInfo>> VerifyIdTokenAsync(string IdToken, CancellationToken ct);
 }
 
 public sealed record GoogleUserInfo(string Subject, string Email, bool EmailVerified);
