55d779ed15cf917bc94b337ba904c0ad9da278dd
mouse1999 <kufreedward26@gmail.com>
2026-05-11 11:29:46 +0100

    feat: comment out the process method

--- worker-java/src/main/java/com/vulnwatch/worker/processors/ScanProcessor.java
+++ worker-java/src/main/java/com/vulnwatch/worker/processors/ScanProcessor.java
@@ -17,24 +17,24 @@ public class ScanProcessor {
     // Add other scanners here: SSL, Headers, etc.
 
     public void process(ScanJob job) {
-        System.out.println("Processing job " + job.getScan_id() + " for domain " + job.getDomain());
-
-        for (String type : job.getScan_type()) {
-            switch (type.toLowerCase()) {
-                case "dns":
-                    dnsScanner.scan(job);
-                    break;
-                case "ssl":
-                    // TODO: Implement SslScanner
-                    System.out.println("SSL scan requested (Not implemented yet)");
-                    break;
-                case "headers":
-                    // TODO: Implement HeaderScanner
-                    System.out.println("Headers scan requested (Not implemented yet)");
-                    break;
-                default:
-                    System.out.println("Unknown scan type: " + type);
-            }
-        }
+//        System.out.println("Processing job " + job.getScan_id() + " for domain " + job.getDomain());
+//
+//        for (String type : job.getScan_type()) {
+//            switch (type.toLowerCase()) {
+//                case "dns":
+//                    dnsScanner.scan(job);
+//                    break;
+//                case "ssl":
+//                    // TODO: Implement SslScanner
+//                    System.out.println("SSL scan requested (Not implemented yet)");
+//                    break;
+//                case "headers":
+//                    // TODO: Implement HeaderScanner
+//                    System.out.println("Headers scan requested (Not implemented yet)");
+//                    break;
+//                default:
+//                    System.out.println("Unknown scan type: " + type);
+//            }
+//        }
     }
 }
